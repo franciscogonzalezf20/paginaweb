44ad93f0733978b3f1180f64bce25cd3d70cbd10
francisco eduardo gonzalez <90358593+franciscogonzalezf20@users.noreply.github.com>
2022-07-08 21:06:15 -0500

    animacion derecha, izquierda, centro

--- java/comandos.js
+++ java/comandos.js
@@ -5,32 +5,58 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("navbar").style.top = "-90px";
   }
   prevScrollpos = currentScrollPos;
 }
 
 /* ///////////////////////////////////////////////////////////////// */
 
-/* animacion aparicion de los elementos */
+/* animacion aparicion de los elementos derecha a izquierda*/
 
-let animado = document.querySelectorAll(".animado");
+let animadoDer = document.querySelectorAll(".animadoDer");
 
 function mostrarScroll(){
   let scrollTop = document.documentElement.scrollTop;
-  for (var i=0; i < animado.length; i++){
-    let alturaAnimado = animado [i].offsetTop
-    if (alturaAnimado - 100 < scrollTop){
-      animado[i].style.opacity = 1;
-      animado[i].classList.add("mostrarDer");
+  for (var i=0; i < animadoDer.length; i++){
+    let alturaAnimado = animadoDer [i].offsetTop
+    if (alturaAnimado - 200 < scrollTop){
+      animadoDer[i].style.opacity = 1;
+      animadoDer[i].classList.add("mostrarDer");
     }
   }
 }
 window.addEventListener('scroll',mostrarScroll);
 
-/* escritura animada */
+/* animacion aparicion de los elementos izquierda a derecha*/
 
-let writing = str => {
-  let arrFromDtr = str.split('');
-  
+let animadoIzq = document.querySelectorAll(".animadoIzq");
+
+function mostrar(){
+  let scrollTop = document.documentElement.scrollTop;
+  for (var i=0; i < animadoIzq.length; i++){
+    let alturaAnima = animadoIzq [i].offsetTop
+    if (alturaAnima - 200 < scrollTop){
+      animadoIzq[i].style.opacity = 1;
+      animadoIzq[i].classList.add("mostrarIzq");
+    }
+  }
 }
+window.addEventListener('scroll',mostrar);
+
+/* animacion aparicion de los elementos arriba hacia abajo*/
+
+
+let animadoUp = document.querySelectorAll(".animadoUp");
+
+function mostrarScr(){
+  let scrollTop = document.documentElement.scrollTop;
+  for (var i=0; i < animadoUp.length; i++){
+    let altura = animadoUp [i].offsetTop
+    if (altura - 200 < scrollTop){
+      animadoUp[i].style.opacity = 1;
+      animadoUp[i].classList.add("mostrarUp");
+    }
+  }
+}
+window.addEventListener('scroll',mostrarScr);
